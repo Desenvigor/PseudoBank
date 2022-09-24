7eaad4032c8be7852e55c10080ff4697a624daa1
Igor Aguiar <desenvigor@gmail.com>
2022-09-24 19:37:41 -0300

    create AccountDAO

---
+++ src/main/java/com/desenvigor/dao/AccountDAO.java
@@ -0,0 +1,45 @@
+package com.desenvigor.dao;
+
+import com.desenvigor.model.Account;
+import com.desenvigor.model.Client;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class AccountDAO {
+    private EntityManager em;
+
+    public AccountDAO(EntityManager em) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pseudoBank");
+        this.em = emf.createEntityManager();
+    }
+
+    public void insert(Account account){
+        em.getTransaction().begin();
+        em.persist(account);
+        em.getTransaction().commit();
+    }
+
+    public Account find(Long id){
+        return em.find(Account.class, id);
+    }
+
+    public void update(Account account) {
+        em.getTransaction().begin();
+        em.persist(account);
+        em.getTransaction().commit();
+    }
+
+    public void delete(Account account){
+        em.getTransaction().begin();
+        em.remove(account);
+        em.getTransaction().commit();
+    }
+
+    public List<Client> findAll(){
+        String jpql = "SELECT c FROM Client c";
+        return em.createQuery(jpql, Client.class).getResultList();
+    }
+}
