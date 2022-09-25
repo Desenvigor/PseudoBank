857f0b12d2523fe10742cba4da84f2033bb21ff6
Igor Aguiar <desenvigor@gmail.com>
2022-09-25 19:05:26 -0300

    Create Employee entity

--- src/main/java/com/desenvigor/model/Employee.java
+++ src/main/java/com/desenvigor/model/Employee.java
@@ -1,2 +1,28 @@
-package com.desenvigor.model;public class Employee {
+package com.desenvigor.model;
+
+import com.desenvigor.dao.ClientDAO;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Entity
+public class Employee extends Client{
+    String credential;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String ssn, Date birthdate, String credential) {
+        super(name, ssn, birthdate);
+        this.credential = credential;
+    }
+
+    public void registerNewClient(Client client){
+        ClientDAO clientDAO = new ClientDAO();
+        clientDAO.insert(client);
+    }
+
+    public boolean authenticate(String credential){
+        return this.credential.equals(credential);
+    }
 }
