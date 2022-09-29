aed2c2599b4661d2294007bc041a407502e82bff
Igor Aguiar <desenvigor@gmail.com>
2022-09-29 20:45:06 -0300

    Create struct to populate database

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,31 +1,31 @@
 import com.desenvigor.dao.AccountDAO;
 import com.desenvigor.dao.ClientDAO;
 import com.desenvigor.dao.EmployeeDAO;
-import com.desenvigor.model.Account;
-import com.desenvigor.model.Client;
+import com.desenvigor.model.*;
 
 public class Program {
     public static void main(String[] args) {
         ClientDAO clientDAO = new ClientDAO();
         AccountDAO accDao = new AccountDAO();
         EmployeeDAO empDAO = new EmployeeDAO();
-        Client client = clientDAO.find(1L);
-        Client client2 = clientDAO.find(2L);
-        Client employee = empDAO.findById(10L);
-
-        Account empAccount = accDao.find(11L);
-        Account clientAccount = accDao.find(8L);
-        Account client2Account = accDao.find(9L);
-
-        System.out.println(client);
-        System.out.println(clientAccount);
-        System.out.println(client2);
-        System.out.println(client2Account);
-        System.out.println(employee);
-        System.out.println(empAccount);
 
 
-        clientAccount.deposit("2.0");
-        accDao.update(clientAccount);
+        //CreateClient
+        Client newClient = new Client();
+        clientDAO.insert(newClient); //Insert client into database
+
+        //Create Savings Account
+        Account newSavAcc = new SavingsAccount();
+        accDao.insert(newSavAcc); //Save account into database
+
+        //Create Current Account
+        Account newCurAcc = new CurrentAccount();
+        accDao.insert(newCurAcc); //Save account into database
+
+        //Create Employee
+        Employee newEmp = new Employee();
+        clientDAO.insert(newEmp);
+
+
     }
 }
